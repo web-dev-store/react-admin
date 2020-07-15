a40b4100a03fab6db6d1b09dff95ba0be6f07f48
sy <sy@sydeMacBook-Pro.local>
2020-07-15 18:20:39 +0800

    feat: first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+import './App.css'
+
+import 'antd/dist/antd.css'
+import { Button } from 'antd'
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<Button type="primary">Button</Button>
+			</header>
+		</div>
+	)
+}
+
+export default App
