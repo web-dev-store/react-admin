8ec1dc8c2bc92ba9f1dfb536d46429512d90b679
ssy <286506460@qq.com>
2020-07-19 02:27:54 +0800

    feat: add grid child page

---
+++ src/pages/grid/info.tsx
@@ -0,0 +1,14 @@
+/*
+ * Author: ssy <286506460@qq.com>
+ * Repository: https://github.com/web-dev-store/react-admin.git
+ * License: MIT
+ * react and antd4 template
+ */
+
+import React from 'react'
+
+const GridInfo: React.FC = () => {
+	return <h1>Info</h1>
+}
+
+export default GridInfo
