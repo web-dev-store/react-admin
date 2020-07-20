37be0cf289b6cb8b34b3aad341c451d5915b68af
ssy <286506460@qq.com>
2020-07-20 14:42:05 +0800

    feat: add 403 page

---
+++ src/pages/error/error-403.tsx
@@ -0,0 +1,14 @@
+/*
+ * Author: ssy <286506460@qq.com>
+ * Repository: https://github.com/web-dev-store/react-admin.git
+ * License: MIT
+ * react and antd4 template
+ */
+
+import React from 'react'
+
+const Error403: React.FC = () => {
+	return <h1>403</h1>
+}
+
+export default Error403
