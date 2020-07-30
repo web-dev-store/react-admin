626b2b64117f62c46794a744d46ebb5a7101e27c
ssy <286506460@qq.com>
2020-07-30 15:05:27 +0800

    feat: add global types

---
+++ src/@types/index.ts
@@ -0,0 +1,12 @@
+/*
+ * Author: ssy <286506460@qq.com>
+ * Repository: https://github.com/web-dev-store/react-admin.git
+ * License: MIT
+ * react and antd4 template
+ */
+
+export interface IPagination {
+	total?: number
+	current: number
+	pageSize: number
+}
