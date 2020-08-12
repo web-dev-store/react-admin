298635d05ba32db8473c920ec9be613ea4ddaa3e
ssy <286506460@qq.com>
2020-08-12 09:53:03 +0800

    fix: should persistStore after inject reducer

--- src/utils/reducerInjectors.js
+++ src/utils/reducerInjectors.js
@@ -1,9 +1,11 @@
 import createReducer from '../redux/reducer'
+import { persistStore } from 'redux-persist'
 
 export function injectReducerFactory(store) {
 	return function injectReducer(key, reducer) {
 		store.injectedReducers[key] = reducer // eslint-disable-line no-param-reassign
 		store.replaceReducer(createReducer(store.injectedReducers))
+		persistStore(store)
 	}
 }
 
